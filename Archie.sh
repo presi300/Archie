80928d4164177359114aa4dac80f0739afe88e57
presi300 <50592132+presi300@users.noreply.github.com>
2021-07-26 22:50:31 +0300

    Add files via upload

---
+++ Archie.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+clear
+echo "Welcome to Archie! A simple terminal based installer for Arch linux"
+echo ""
+echo "Before we begin make sure all your partitions are formatted and mounted"
+echo "Press enter to begin"
+read pressenter
+
+#TimedatectNTP
+
+timedatectl set-ntp true
+
+#Country
+
+echo ""
+echo ""
+echo "Enter which country you are in (1st letter should be capital)"
+read country
+
+#Reflector
+
+pacman -S reflector --noconfirm
+reflector -c $country -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
+#Install Arch Linux
+
+pacstrap /mnt base linux linux-firmware base-devel nano
+
+#Stab!
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+#Clone Archie2
+
+git clone https://github.com/presi300/Archie2 /mnt/Archie2
+cp /mnt/Archie2/Archie2.sh /mnt
+chmod +rx /mnt/Archie2.sh
+rm -rf /mnt/Archie2
+
+#Chroot
+
+arch-chroot /mnt /bin/bash -c ./Archie2.sh
+
+#Proceed to Archie2
+
+
+
+
